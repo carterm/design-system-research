cbfaea7b9cd5f33839e7da76d0cd50a8ae1e4bf3
Carter Medlin <carterm@users.noreply.github.com>
2023-11-07 12:17:20 -0800

    basic head

--- src/js/index.js
+++ src/js/index.js
@@ -14,4 +14,24 @@ window.addEventListener("load", () => {
   }
 
   window.customElements.define("ca-header", MyElement2);
+
+  class ca_head extends HTMLElement {
+    connectedCallback() {
+      if (this.dataset.title) {
+        document.title = this.dataset.title;
+      }
+
+      if (this.dataset.description) {
+        const metaDescription =
+          document.head.querySelector(`meta[name="description" i]`) ||
+          Object.assign(document.createElement("meta"), {
+            name: "Description"
+          });
+
+        metaDescription.attributes["content"].value = this.dataset.description;
+      }
+    }
+  }
+
+  window.customElements.define("ca-head", ca_head);
 });
