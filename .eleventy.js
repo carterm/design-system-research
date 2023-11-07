1ad2674f923bf764a8bb87f4bd60e542066f0962
Carter Medlin <carterm@users.noreply.github.com>
2023-11-06 16:45:44 -0800

    start

---
+++ .eleventy.js
@@ -0,0 +1,53 @@
+//@ts-check
+const defaultConfig = require("@11ty/eleventy/src/defaultConfig");
+const path = require("path");
+
+module.exports = function (
+  /** @type {import("@11ty/eleventy").UserConfig} **/ eleventyConfig
+) {
+  eleventyConfig.addPassthroughCopy({
+    "sample_site/images": "images",
+    "sample_site/siteRoot": "/",
+    "src/js": "js"
+  });
+
+  //Start with default config, easier to configure 11ty later
+  const config = defaultConfig(eleventyConfig);
+
+  // allow nunjucks templating in .html files
+  config.htmlTemplateEngine = "njk";
+  config.markdownTemplateEngine = "njk";
+  config.templateFormats = ["html", "njk", "11ty.js", "md"];
+
+  config.dir = {
+    // site content pages
+    input: "sample_site/pages",
+    // site structure pages (path is realtive to input directory)
+    data: "../_data",
+    includes: "../_includes",
+    // site final outpuut directory
+    output: "_site"
+  };
+
+  //Adding a transform to make the output work as non-server static files
+  eleventyConfig.addTransform(
+    "staticPaths",
+    /**
+     * @param {string} content
+     * @param {string} outputPath
+     */
+    async function (content, outputPath) {
+      const basePath = config.dir.output;
+
+      const relativePath = path
+        .relative(path.dirname(outputPath), path.dirname(basePath))
+        .slice(0, -2); //removing last 2 dots
+
+      return content
+        .replace(/href="(.*\/)"/g, 'href="$1index.html"') // fixing any root path links
+        .replace(/=\"\//g, `="${relativePath}`); //Replace all ... ="/  ... with new path
+    }
+  );
+
+  return config;
+};
