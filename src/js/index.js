7ef00767b9049c6f17d29dc631272a327a16cd81
Carter Medlin <carterm@users.noreply.github.com>
2023-11-06 17:00:00 -0800

    starter component

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,17 @@
 //@ts-check
 
-console.log("script attached");
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_Components/Using_custom_elements
+
+window.addEventListener("load", () => {
+  class MyElement extends HTMLElement {
+    connectedCallback() {
+      // Create a shadow root
+      // const shadow = this.attachShadow({ mode: "open" });
+
+      this.style.backgroundColor = "#FAA";
+      this.innerText = "connected";
+    }
+  }
+
+  window.customElements.define("ca-eureka", MyElement);
+});
