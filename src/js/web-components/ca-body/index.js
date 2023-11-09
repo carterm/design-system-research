e9d4ab68180d81c1e9cf02aa0108131b33703df4
Carter Medlin <carterm@users.noreply.github.com>
2023-11-09 12:41:22 -0800

    style update

--- src/js/web-components/ca-body/index.js
+++ src/js/web-components/ca-body/index.js
@@ -5,4 +5,12 @@ export default class ca_body extends ca_eureka_component {
   connectedCallback() {
     this.requireParentElement(ca_eureka.tagName);
   }
+
+  /**
+   *
+   * @param {Window} window
+   */
+  static defineComponent(window) {
+    window.customElements.define(this.tagName, this, { extends: "p" });
+  }
 }
