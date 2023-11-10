048fb3b2afe6493c7c912bd4a0525cd8474bfba2
Carter Medlin <carterm@users.noreply.github.com>
2023-11-09 16:00:27 -0800

    starting with nav

--- src/js/web-components/ca-nav/index.js
+++ src/js/web-components/ca-nav/index.js
@@ -1,7 +1,7 @@
 //@ts-check
 import { ca_eureka_component } from "../index.js";
 
-export default class ca_header extends ca_eureka_component {
+export default class ca_nav extends ca_eureka_component {
   connectedCallback() {
     this.requireParentElement("ca-eureka");
     this.requireSingle();
--- src/js/web-components/index.js
+++ src/js/web-components/index.js
@@ -2,15 +2,8 @@
 import ca_eureka_component from "./ca-eureka-component/index.js";
 import ca_eureka from "./ca-eureka/index.js";
 import ca_head from "./ca-head/index.js";
-import ca_header from "./ca-header/index.js";
+import ca_nav from "./ca-nav/index.js";
 import ca_body from "./ca-body/index.js";
 import ca_footer from "./ca-footer/index.js";
 
-export {
-  ca_eureka_component,
-  ca_eureka,
-  ca_head,
-  ca_header,
-  ca_body,
-  ca_footer
-};
+export { ca_eureka_component, ca_eureka, ca_head, ca_nav, ca_body, ca_footer };
