0305b373386f05663c5e6d3ebb70ab49e4acdf06
Carter Medlin <carterm@users.noreply.github.com>
2023-11-14 16:43:36 -0800

    simplified

--- src/js/index.js
+++ src/js/index.js
@@ -2,18 +2,12 @@
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Web_Components/Using_custom_elements
 
-import {
-  ca_eureka,
-  ca_head,
-  ca_nav,
-  ca_body,
-  ca_footer
-} from "./web-components/index.js";
+import * as eureka from "./web-components/index.js";
 
 window.addEventListener("load", () => {
-  ca_eureka.defineComponent(window);
-  ca_head.defineComponent(window);
-  ca_nav.defineComponent(window);
-  ca_body.defineComponent(window);
-  ca_footer.defineComponent(window);
+  eureka.ca_eureka.defineComponent(window);
+  eureka.ca_head.defineComponent(window);
+  eureka.ca_nav.defineComponent(window);
+  eureka.ca_body.defineComponent(window);
+  eureka.ca_footer.defineComponent(window);
 });
--- src/js/web-components/ca-eureka/index.js
+++ src/js/web-components/ca-eureka/index.js
@@ -1,6 +1,15 @@
 //@ts-check
 import ca_eureka_component from "../ca-eureka-component/index.js";
 
+/**
+ * Base element for Eureka
+ * @example
+ * ```html
+ * <ca-eureka>
+ *
+ * </ca-eureka>
+ * ```
+ */
 export default class ca_eureka extends ca_eureka_component {
   connectedCallback() {
     this.requireParentElement("body");
