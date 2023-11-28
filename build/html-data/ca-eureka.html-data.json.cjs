b38f654409ca8ff967ae3c28f48f391bd5447012
Carter Medlin <carterm@users.noreply.github.com>
2023-11-28 13:46:48 -0800

    cleanup

--- build/html-data/ca-eureka.html-data.json.cjs
+++ build/html-data/ca-eureka.html-data.json.cjs
@@ -10,48 +10,50 @@ const path = require("path");
  * Default root for html-data.json
  * @typedef {Object} html_data
  * @property {number} version
- * @property {*[]} tags
+ * @property {{}[]} tags
  */
 
-/** @type {html_data} */
+/**
+ * Starting template for html-data.json
+ * @type {html_data} */
 const combo = {
   version: 1.1,
   tags: []
 };
 
-const replacements = fs
-  .readdirSync(srcPath, {
-    recursive: true,
-    withFileTypes: true
-  })
-  .filter(d => d.name.startsWith(".html-data.") && d.name.endsWith(".md"));
-
-// process all the html-data files
-fs.readdirSync(srcPath, {
+const allFiles = fs.readdirSync(srcPath, {
   recursive: true,
   withFileTypes: true
-})
+});
+
+// process all the html-data files
+allFiles
   .filter(d => d.name.endsWith(".html-data.json"))
-  .map(f => {
+  .forEach(f => {
     let jsonText = fs.readFileSync(f.path + "/" + f.name, {
       encoding: "utf-8"
     });
 
-    replacements
-      .filter(r => r.path === f.path)
+    allFiles
+      .filter(
+        d =>
+          d.path === f.path &&
+          d.name.startsWith(".html-data.") &&
+          d.name.endsWith(".md")
+      )
       .forEach(r => {
-        const mdText = JSON.stringify(
-          fs.readFileSync(r.path + "/" + r.name, {
-            encoding: "utf-8"
-          })
+        jsonText = jsonText.replace(
+          `"${r.name}"`,
+          JSON.stringify(
+            fs.readFileSync(r.path + "/" + r.name, {
+              encoding: "utf-8"
+            })
+          )
         );
-
-        jsonText = jsonText.replace(`"${r.name}"`, mdText);
       });
 
-    return /** @type {html_data} */ (JSON.parse(jsonText));
-  })
-  .forEach(f => combo.tags.push(...f.tags));
+    combo.tags.push(.../** @type {html_data} */ (JSON.parse(jsonText)).tags);
+  });
 
 const dir = path.dirname(targetFileName);
 !fs.existsSync(dir) && fs.mkdirSync(dir, { recursive: true });
