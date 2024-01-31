1e1ed80dd13e211361d011471a74e76089ffcbac
jon-grant-state-ca-gov <jon.grant@state.ca.gov>
2024-01-31 07:23:39 -0800

    Updates for shadow DOM in base class

--- src/js/web-components/ca-header/index.js
+++ src/js/web-components/ca-header/index.js
@@ -2,10 +2,10 @@
 import ca_eureka_component from "../ca-eureka-component/index.js";
 
 // @ts-ignore
-import CssStyleString from "./styles.css" assert { type: "css" };
+import css from "./styles.css";
 
 // @ts-ignore
-import HtmlTemplateString from "./template.html" assert { type: "html" };
+import html from "./template.html";
 
 export default class ca_header extends ca_eureka_component {
   /** @override */
@@ -16,19 +16,30 @@ export default class ca_header extends ca_eureka_component {
   /** @protected @readonly @override */
   static observedAttributes = ["data-department"];
 
+  /**
+   * @param {string} name
+   * @param {string} _oldValue
+   * @param {string} newValue
+   * @protected
+   * @override
+   */
+  attributeChangedCallback(name, _oldValue, newValue) {
+    const o = ca_header.observedAttributes;
+    console.log("attribute changed: ", o);
+    console.log(this);
+    switch (name) {
+      case o[0]: //"data-department":
+        this.slot = `${newValue}`;
+        break;
+      default:
+        this.slot = `Department of Website`;
+    }
+  }
   constructor() {
     super({
-      parent: "ca-root"
+      shadow: true,
+      css,
+      html
     });
-
-    const shadow = this.attachShadow({ mode: "open" });
-
-    this.addStyle();
-    this.addStyle(CssStyleString);
-
-    const myTemplate = document.createElement("template");
-    myTemplate.innerHTML = this.setHTMLTemplateString(HtmlTemplateString);
-
-    shadow.appendChild(myTemplate.content.cloneNode(true));
   }
 }
--- src/js/web-components/starter-component/index.js
+++ src/js/web-components/starter-component/index.js
@@ -2,10 +2,10 @@
 import ca_eureka_component from "../ca-eureka-component/index.js";
 
 // @ts-ignore
-import CssStyleString from "./styles.css" assert { type: "css" };
+import css from "./styles.css";
 
 // @ts-ignore
-import HtmlTemplateString from "./template.html" assert { type: "html" };
+import html from "./template.html";
 
 export default class my_component extends ca_eureka_component {
   /** @override */
@@ -18,17 +18,9 @@ export default class my_component extends ca_eureka_component {
 
   constructor() {
     super({
-      parent: "ca-parent-element"
+      shadow: true,
+      css,
+      html
     });
-
-    const shadow = this.attachShadow({ mode: "open" });
-
-    this.addStyle();
-    this.addStyle(CssStyleString);
-
-    const myTemplate = document.createElement("template");
-    myTemplate.innerHTML = this.setHTMLTemplateString(HtmlTemplateString);
-
-    shadow.appendChild(myTemplate.content.cloneNode(true));
   }
 }
