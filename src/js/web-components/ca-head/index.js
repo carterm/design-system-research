8d6818c52b182ee95339a35160f55ed696d0ce32
Carter Medlin <carterm@users.noreply.github.com>
2024-02-08 14:23:09 -0800

    using constructor for ca-head

--- src/js/web-components/ca-head/index.js
+++ src/js/web-components/ca-head/index.js
@@ -16,50 +16,52 @@ export default class ca_head extends ca_eureka_component {
     return ["data-title", "data-description"];
   }
 
-  /**
-   *
-   * @param {string} name
-   * @param {string} _oldValue
-   * @param {string} newValue
-   * @override
-   */
-  // eslint-disable-next-line class-methods-use-this
-  attributeChangedCallback(name, _oldValue, newValue) {
+  constructor() {
     /**
-     * @param {string} metaName
-     * @param {string} metaValue
+     *
+     * @param {string} name
+     * @param {string} _oldValue
+     * @param {string} newValue
      */
-    const setMeta = (metaName, metaValue) => {
-      const existingMeta = document.head.querySelector(
-        `meta[name="${metaName}" i]`
-      );
-
-      if (existingMeta) {
-        existingMeta.attributes["content"].value = newValue;
-      } else {
-        document.head.append(
-          Object.assign(document.createElement("meta"), {
-            name: metaName,
-            content: metaValue
-          })
+    const attributeChangedCallback = (name, _oldValue, newValue) => {
+      /**
+       * @param {string} metaName
+       * @param {string} metaValue
+       */
+      const setMeta = (metaName, metaValue) => {
+        const existingMeta = document.head.querySelector(
+          `meta[name="${metaName}" i]`
         );
+
+        if (existingMeta) {
+          existingMeta.attributes["content"].value = newValue;
+        } else {
+          document.head.append(
+            Object.assign(document.createElement("meta"), {
+              name: metaName,
+              content: metaValue
+            })
+          );
+        }
+      };
+
+      switch (name) {
+        case "data-title":
+          document.title = newValue;
+          ["title", "og:title", "twitter:title"].forEach(m =>
+            setMeta(m, newValue)
+          );
+
+          break;
+        case "data-description":
+          ["description", "og:description", "twitter:description"].forEach(m =>
+            setMeta(m, newValue)
+          );
+
+          break;
       }
     };
 
-    switch (name) {
-      case "data-title":
-        document.title = newValue;
-        ["title", "og:title", "twitter:title"].forEach(m =>
-          setMeta(m, newValue)
-        );
-
-        break;
-      case "data-description":
-        ["description", "og:description", "twitter:description"].forEach(m =>
-          setMeta(m, newValue)
-        );
-
-        break;
-    }
+    super({ attributeChangedCallback });
   }
 }
