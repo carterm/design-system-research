281892d2c386fc782b51f5e5bde847b984ac29ed
Carter Medlin <carterm@users.noreply.github.com>
2024-02-09 15:33:58 -0800

    cards working

--- src/js/web-components/ca-cardgroup-wrap/index.js
+++ src/js/web-components/ca-cardgroup-wrap/index.js
@@ -1,14 +1,17 @@
 //@ts-check
 
+// @ts-ignore
+import css from "./styles.css";
+
 import ca_eureka_component from "../_ca-eureka-component/index";
 
-export default class ca_cardgroup_wrap extends ca_eureka_component {
+export default class extends ca_eureka_component {
   /** @override */
   static get tagName() {
     return "ca-cardgroup-wrap";
   }
 
   constructor() {
-    super({ shadow: true });
+    super({ shadow: true, css });
   }
 }
