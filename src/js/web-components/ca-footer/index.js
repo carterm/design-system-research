cf78efaeb603eb01d98da9f4d10b1baf08d6e0ae
Carter Medlin <carterm@users.noreply.github.com>
2024-02-09 15:24:53 -0800

    reducing custom class name req

--- src/js/web-components/ca-footer/index.js
+++ src/js/web-components/ca-footer/index.js
@@ -7,7 +7,7 @@ import css from "./styles.css";
 // @ts-ignore
 import html from "./template.html";
 
-export default class ca_footer extends ca_eureka_component {
+export default class extends ca_eureka_component {
   /** @override */
   static get tagName() {
     return "ca-footer";
