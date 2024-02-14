f9310d4d5feeb0509172a811a49fdab64449c1d7
Carter Medlin <carterm@users.noreply.github.com>
2024-02-14 15:04:20 -0800

    using sync for

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -6,7 +6,8 @@ import * as all from "./index";
 
 //comment out any elements you are not using
 //Definition order matters!!!  Code will run in this order
-[
+
+const my_bundle = [
   all.ca_custom_css,
   all.ca_seo,
   all.ca_nav,
@@ -14,4 +15,9 @@ import * as all from "./index";
   all.ca_accordion,
   all.ca_card_simple,
   all.ca_cardgroup_wrap
-].forEach(c => window.customElements.define(c.tagName, c));
+];
+
+for (const c of my_bundle) {
+  //sync "for", to ensure define order
+  window.customElements.define(c.tagName, c);
+}
