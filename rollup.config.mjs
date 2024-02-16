d3eac21841621fea67ec2c7bd39ab33bdece376e
Carter Medlin <carterm@users.noreply.github.com>
2024-02-15 17:04:46 -0800

    rollup cleanup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -17,7 +17,6 @@ export default function () {
     module: true,
     mangle: {
       properties: {
-        builtins: false,
         reserved: [
           "observedAttributes",
           "connectedCallback",
