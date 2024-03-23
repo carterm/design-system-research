07fedcadbd90eb7316141a4c51834a900266c97a
Carter Medlin <carterm@users.noreply.github.com>
2024-03-23 10:45:01 -0700

    working on cards

--- components/cal-ds-card/index.js
+++ components/cal-ds-card/index.js
@@ -3,9 +3,6 @@
 // @ts-ignore
 import css from "./styles.css";
 
-// @ts-ignore
-import html from "./template.html";
-
 import cal_ds_base from "../_cal-ds-base/index";
 
 export default class extends cal_ds_base {
@@ -15,6 +12,24 @@ export default class extends cal_ds_base {
   }
 
   constructor() {
-    super({ shadow: true, css, html });
+    const _contentChanged = () => {
+      const myTemplate = this.template;
+
+      if (myTemplate && this.shadowRoot) {
+        const li = document.createElement("li");
+        const div = document.createElement("div");
+
+        div.appendChild(myTemplate.content.cloneNode(true));
+        li.appendChild(div);
+
+        div.querySelectorAll(":scope > img").forEach(i => li.appendChild(i));
+
+        this.shadowRoot.appendChild(li);
+      }
+    };
+
+    super({ shadow: true, css, connectedCallback: _contentChanged });
+
+    this.observeTemplate(_contentChanged);
   }
 }
