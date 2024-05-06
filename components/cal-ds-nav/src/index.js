9bf3438694587ebabd60ca708f9fa7e46e52114f
Carter Medlin <carterm@users.noreply.github.com>
2024-05-06 15:21:19 -0700

    templateChangedCallback in base

--- components/cal-ds-nav/src/index.js
+++ components/cal-ds-nav/src/index.js
@@ -55,25 +55,11 @@ export default class extends cal_ds_base {
       }
     };
 
-    super({ shadow: true, css, connectedCallback: _contentChanged });
-
-    if (this.UserTemplate) {
-      // Callback function to execute when mutations are observed
-      // eslint-disable-next-line jsdoc/no-undefined-types
-      /** @type {MutationCallback} */
-      const mutationCallback = mutationsList =>
-        mutationsList.forEach(_contentChanged);
-
-      // Create an observer instance linked to the callback function
-      const observer = new MutationObserver(mutationCallback);
-
-      // Start observing the target node for configured mutations
-      observer.observe(this.UserTemplate, {
-        attributes: true,
-        childList: true,
-        subtree: true,
-        characterData: true
-      });
-    }
+    super({
+      shadow: true,
+      css,
+      connectedCallback: _contentChanged,
+      templateChangedCallback: _contentChanged
+    });
   }
 }
