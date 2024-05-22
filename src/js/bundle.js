422b15b0f5c8865e21bc37f4ccf047d7378106ea
Carter Medlin <carterm@users.noreply.github.com>
2024-05-22 11:08:39 -0700

    bring back bundle

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -8,15 +8,15 @@ import * as all from "./index";
 //Definition order matters!!!  Code will run in this order
 
 const my_bundle = [
-  //all.cal_ds_custom_css,
-  //all.cal_ds_seo,
-  //all.cal_ds_nav,
-  //all.cal_ds_footer,
-  all.cal_ds_accordion
-  //all.cal_ds_card_simple,
-  //all.cal_ds_cardgroup_wrap,
-  //all.cal_ds_dev_editor,
-  //all.cal_ds_banner
+  all.cal_ds_custom_css,
+  all.cal_ds_seo,
+  all.cal_ds_nav,
+  all.cal_ds_footer,
+  all.cal_ds_accordion,
+  all.cal_ds_card_simple,
+  all.cal_ds_cardgroup_wrap,
+  all.cal_ds_dev_editor,
+  all.cal_ds_banner
 ];
 
 for (const c of my_bundle) {
