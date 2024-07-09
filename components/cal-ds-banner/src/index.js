57e230bbf01478025293d58415426da13021eb29
Carter Medlin <carterm@users.noreply.github.com>
2024-07-09 15:56:02 -0700

    code reductions

--- components/cal-ds-banner/src/index.js
+++ components/cal-ds-banner/src/index.js
@@ -38,24 +38,21 @@ export default class my extends cal_ds_base {
     };
 
     const _contentChanged = () => {
-      if (this.UserTemplate && this.shadowRoot && this.HTMLTemplateString) {
-        this.shadowRoot.innerHTML = this.HTMLTemplateString;
+      const _UserTemplate = this.UserTemplate;
+      const _shadowRoot = this.shadowRoot;
+      const _HTMLTemplateString = this.HTMLTemplateString;
+      const _dataTarget = this.dataset.target;
 
-        const ul = /** @type {HTMLElement} */ (
-          this.shadowRoot.querySelector("p")
-        );
-        const dom = /** @type {DocumentFragment} */ (
-          this.UserTemplate.cloneNode(true)
-        );
+      if (_UserTemplate && _shadowRoot && _HTMLTemplateString) {
+        _shadowRoot.innerHTML = _HTMLTemplateString;
 
-        ul.appendChild(dom);
+        /** @type {HTMLElement} */ (_shadowRoot.querySelector("p")).appendChild(
+          _UserTemplate.cloneNode(true)
+        );
       }
 
-      const target = document.querySelector(
-        this.dataset.target || ":scope > body"
-      );
-      if (!target)
-        throw new Error(`Can't find data-target - ${this.dataset.target}`);
+      const target = document.querySelector(_dataTarget || ":scope > body");
+      if (!target) throw new Error(`Can't find data-target - ${_dataTarget}`);
 
       //Move itself to the target
       if (target.firstElementChild !== this) target.prepend(this);
