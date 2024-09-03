db252dff565417aa1f4306acea815d43e129b37e
Carter Medlin <carterm@users.noreply.github.com>
2024-09-03 13:24:30 -0700

    observed

--- components/cal-ds-header/src/index.js
+++ components/cal-ds-header/src/index.js
@@ -21,7 +21,7 @@ export default class my extends cal_ds_base {
    * @override
    */
   static get observedAttributes() {
-    return [];
+    return ["data-logo-overflow"];
   }
 
   constructor() {
